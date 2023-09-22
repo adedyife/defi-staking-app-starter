81bca1cb15c288b695e5e3274f9d3eee401f60f0
Pacifire <adedayoifeoluwas@gmail.com>
2023-09-22 19:41:27 +0000

    init

---
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+
+contract Tether {
+    string public name = "mock Tether";
+    string public symbol = "mUSDT";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8 public decimals = 18;
+}
