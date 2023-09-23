4451e192598ab02e99fd06f56146a530fac233a5
Pacifire <adedayoifeoluwas@gmail.com>
2023-09-23 20:59:01 +0000

    Update decentralBank.tests.js

--- defi-staking-app-starter/tests/decentralBank.tests.js
+++ defi-staking-app-starter/tests/decentralBank.tests.js
@@ -1,1 +1,19 @@
+const { assert } = require("console");
 
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("DecentralBank", (accounts) => {
+  describe("Mock tether deployment", async () => {
+    it("matches name successfully", async () => {
+      let tether = await Tether.new();
+      const name = tether.name();
+      assert.equal(name, "Mock tether");
+    });
+  });
+});
