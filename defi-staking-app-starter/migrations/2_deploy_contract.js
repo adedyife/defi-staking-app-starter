c5369cf11db908fbef78555c926d79f2a9916cec
Pacifire <adedayoifeoluwas@gmail.com>
2023-09-23 15:46:41 +0000

    Update 2_deploy_contract.js

--- defi-staking-app-starter/migrations/2_deploy_contract.js
+++ defi-staking-app-starter/migrations/2_deploy_contract.js
@@ -1,10 +1,21 @@
+import DecentralBank from '../src/truffle_abis/DecentralBank.json';
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-const Tether = artifacts.require("Tether")
-const RWD = artifacts.require("RWD")
-const DecentralBank = artifacts.require("DecentralBank")
+module.exports = async function(deployer, network, accounts) {
+  // deploy contracts
+  await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
 
-module.exports = async function(deployer, network, account) {
-   await deployer.deploy(Tether)
-   await deployer.deploy(RWD)
-   await deployer.deploy(DecentralBank)
-}
+  await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const Decentralbank = await DecentralBank.deployed();
+
+  // transfer all RWD tokens to DecentralBank
+  await rwd.transfer(Decentralbank.address, "1000000000000000000000000");
+
+  // distribute 100 tether to investors
+  tether.transfer(accounts[1] ,"100000000000000000000")
+};
