f02b5b8f3613a7de86947c8b76a063ee15214cdc
Pacifire <adedayoifeoluwas@gmail.com>
2023-09-23 01:33:14 +0000

    push

--- defi-staking-app-starter/migrations/2_deploy_contract.js
+++ defi-staking-app-starter/migrations/2_deploy_contract.js
@@ -1,5 +1,10 @@
-const Tether = artifacts.require("Tether")
 
-module.exports = async function(deployer) {
+const Tether = artifacts.require("Tether")
+const RWD = artifacts.require("RWD")
+const DecentralBank = artifacts.require("DecentralBank")
+
+module.exports = async function(deployer, network, account) {
    await deployer.deploy(Tether)
+   await deployer.deploy(RWD)
+   await deployer.deploy(DecentralBank)
 }
